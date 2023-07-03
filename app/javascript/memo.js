c316e573d49f3c11e1b20407b698d8db02953f22
chicorycoffee <yk.nonogaki@gmail.com>
2023-07-03 19:50:35 +0900

    コードの整理

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,3 +1,17 @@
+const buildHTML = (XHR) => { //buildHTML関数を定義・HTMLの生成部分の切り出し
+  const item = XHR.response.post;//投稿されたメモの情報を抽出
+  const html = ` 
+  <div class="post">
+   <div class="post-date">
+     投稿日時：${item.created_at}
+   </div>
+   <div class="post-content">
+     ${item.content}
+   </div>
+ </div>`; //テンプレートリテラルでHTMLの要素を作成できる
+ return html; //buildHTMLの返り値にHTMLを指定（3~11行）
+}
+
 function post (){
   const submit = document.getElementById("submit");//submitの要素を取得
   submit.addEventListener("click", (e) => { //クリックのイベント発火の際に実行する関数を定義
@@ -9,20 +23,14 @@ function post (){
     XHR.responseType = "json"; //レスポンスのデータフォーマットをJsonに指定
     XHR.send(formData); //リクエストを送信、フォームの内容を送信
     XHR.onload = () => { //onloadプロパティ＝リクエストの送信が成功した時に呼び出される
-    const list = document.getElementById("list");
+    if (XHR.status != 200){ //HTTPステータスコードが格納
+      alert(`Error ${XHR.status}: ${XHR.statusText}`);
+      return null;//javascriptの処理から抜ける
+    }
+      const list = document.getElementById("list");
     const formText = document.getElementById("content");
-    const item = XHR.response.post;//投稿されたメモの情報を抽出
-    const html = ` 
-         <div class="post">
-          <div class="post-date">
-            投稿日時：${item.created_at}
-          </div>
-          <div class="post-content">
-            ${item.content}
-          </div>
-        </div>`; //テンプレートリテラルでHTMLの要素を作成できる
-        list.insertAdjacentHTML("afterend", html); //listに格納された要素の直後に生成したHTMLを挿入
-        formText.value = ""; //formTextのvalue属性にからの文字列を指定
+    list.insertAdjacentHTML("afterend", buildHTML); //listに格納された要素の直後に生成したHTMLを挿入
+    formText.value = ""; //formTextのvalue属性にからの文字列を指定
     };
   });
 };
